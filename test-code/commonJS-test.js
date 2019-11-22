4a8ea094c06753e6187416e96b57d30d96a97fc0
Emmanuel Chappat <m4nu@hotmail.fr>
2019-11-22 20:55:54 +0100

    Test hook

--- test-code/commonJS-test.js
+++ test-code/commonJS-test.js
@@ -1,21 +1,7 @@
 /**
- * Dispatch a request to the server using the configured adapter.
- * second line () with *3 Weird 75% $# symbols!~`.
- *
- * @param {Type} withType This one has a type.
- * @param noType This one does not have a type.
- * @returns {Promise} The Promise to be fulfilled
- */
-module.exports = function dispatchRequest(config) {
-  var iAmAVriable = "Pouette pouette";
-
-  return "yeah";
-};
-
-/**
  * @returns Just a hard coded string.
  */
-module.exports = function(config) {
+module.exports = function IamAnNewFunction(config) {
   var iAmAVriable = "Pouette pouette";
 
   return "yeah";
