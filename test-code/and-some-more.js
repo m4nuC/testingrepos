871c2aaaa514896f15d97cf8726eebd03b7e6186
Emmanuel Chappat <m4nu@hotmail.fr>
2019-11-23 15:04:53 +0100

    Testing

---
+++ test-code/and-some-more.js
@@ -0,0 +1,15 @@
+/**
+ * @returns Just a hard coded string.
+ */
+module.exports = function IamAnNewFunction(config) {
+  var iAmAVriable = "Pouette pouette";
+
+  return "yeah";
+};
+
+// Just some hand written comment
+module.exports = function inlineComment(config) {
+  var iAmAVriable = "Pouette pouette";
+
+  return "yeah";
+};
